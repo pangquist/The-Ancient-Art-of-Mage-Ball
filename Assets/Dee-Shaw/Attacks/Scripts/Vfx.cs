3367cabd2215b9a27369fa520528a403aed4ef6a
valter100 <va6648li-s@student.lu.se>
2021-03-31 10:43:07 +0200

    Revert "Revert "Added Unity projekt""
    This reverts commit fd9db5516eb4796d61a30b649c3d4d2e56c3e6b3.

---
+++ Assets/Dee-Shaw/Attacks/Scripts/Vfx.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vfx : MonoBehaviour
+{
+    public GameObject Splash;
+    public AudioSource HitSource;
+    
+    void Start()
+    {
+        HitSource = GetComponent<AudioSource>();
+
+        
+    }
+    
+
+    void OnCollisionEnter(Collision co)
+    {
+        ContactPoint contact = co.contacts[0];
+
+        Vector3 pos = contact.point;
+        Quaternion rot = Quaternion.FromToRotation(Vector3.down, contact.point);
+        HitSource.Play();
+        
+
+        GameObject e = Instantiate(Splash);
+
+        e.transform.position = pos;
+        e.transform.rotation = rot;
+        
+        
+        
+
+
+    }
+
+    
+
+
+}
