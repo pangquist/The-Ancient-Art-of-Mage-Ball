b05c2918ba1d4bfd1003e1ef0d4d961fa66b016f
emilpasse <emil@passe.se>
2021-03-31 15:46:37 +0200

    fix

---
+++ Assets/Dee-Shaw/Attacks/Scripts/camerashake.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerashake : MonoBehaviour
+{
+    public  GameObject  Camera; // set this via inspector
+  public float shake = 0f;
+  public float shakeAmount  = 0.2f;
+   public float decreaseFactor = 1.0f;
+ 
+   void  Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))                                              //  (shake > 0)
+        {
+            Camera.transform.localPosition = Random.insideUnitSphere * shakeAmount;
+            shake -= Time.deltaTime * decreaseFactor;
+
+        }
+        else
+        {
+            shake = 0.0f;
+        }
+    }
+}
---
+++ Assets/Playground/Scripts/DestroyAfter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    public float lifeLength;
+    void Start()
+    {
+        Destroy(gameObject, lifeLength);
+    }
+}
