0770e3e721cd78ca4b21da2b239e35fcc3a42cd6
valter100 <va6648li-s@student.lu.se>
2021-04-03 19:32:59 +0200

    Steam integration

--- Assets/Scripts/Main menu/MainMenu.cs
+++ Assets/Scripts/Main menu/MainMenu.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using Steamworks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,66 +10,66 @@ public class MainMenu : MonoBehaviour
 
     [SerializeField] bool useSteam = false;
 
-    //protected Callback<LobbyCreated_t> lobbyCreated;
-    //protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
-    //protected Callback<LobbyEnter_t> lobbyEntered;
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
 
-    //private void Start()
-    //{
-    //    if (!useSteam)
-    //        return;
+    private void Start()
+    {
+        if (!useSteam)
+            return;
 
-    //    lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
-    //    gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
-    //    lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
-    //}
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
 
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
 
-        //if (useSteam)
-        //{
-        //    SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
-        //    return;
-        //}
+        if (useSteam)
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
 
         NetworkManager.singleton.StartHost();
-        //}
+    }
 
-        //void OnLobbyCreated(LobbyCreated_t callback)
-        //{
-        //    if (callback.m_eResult != EResult.k_EResultOK)
-        //    {
-        //        landingPagePanel.SetActive(true);
-        //        return;
-        //    }
+    void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
 
-        //    NetworkManager.singleton.StartHost();
+        NetworkManager.singleton.StartHost();
 
-        //    SteamMatchmaking.SetLobbyData(
-        //        new CSteamID(callback.m_ulSteamIDLobby),
-        //        "HostAdDress",
-        //        SteamUser.GetSteamID().ToString());
-        //}
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAdDress",
+            SteamUser.GetSteamID().ToString());
+    }
 
-        //void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
-        //{
-        //    SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
-        //}
+    void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
 
-        //void OnLobbyEntered(LobbyEnter_t callback)
-        //{
-        //    if (NetworkServer.active)
-        //        return;
+    void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active)
+            return;
 
-        //    string hostAddress = SteamMatchmaking.GetLobbyData(
-        //        new CSteamID(callback.m_ulSteamIDLobby),
-        //        "HostAddress");
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress");
 
-        //    NetworkManager.singleton.networkAddress = hostAddress;
-        //    NetworkManager.singleton.StartClient();
+        NetworkManager.singleton.networkAddress = hostAddress;
+        NetworkManager.singleton.StartClient();
 
-        //    landingPagePanel.SetActive(false);
+        landingPagePanel.SetActive(false);
     }
 }
