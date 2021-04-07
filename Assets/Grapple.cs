e0ac1c0283a57184f870b2b04d45749b568b6357
pangquist <par.angquist@gmail.com>
2021-04-07 13:26:36 +0200

    First version of the chain casters grapple

---
+++ Assets/Grapple.cs
@@ -0,0 +1,82 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : NetworkBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    [SerializeField] LayerMask grappleable;
+    [SerializeField] Transform castPoint, camera, player;
+    private float maxRange = 25f;
+
+    PlayerMovement playerMovement;
+    [SerializeField]
+    float grappleSpeed;
+
+    [SerializeField]
+    float cooldown;
+    float timeSinceLastCast;
+    [SerializeField]
+    bool isOnCooldown;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+
+        lr.positionCount = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+
+        if (lr.positionCount == 0) return;
+
+        Vector3 grappleVector = grapplePoint - player.position;
+
+        playerMovement.controller.Move(grappleVector * grappleSpeed * 2 * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        DrawChain();
+    }
+
+    void StartGrapple()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(camera.position, camera.forward, out hit, maxRange))
+        {
+            grapplePoint = hit.point;
+
+            lr.positionCount = 2;
+        }
+    }
+
+    void DrawChain()
+    {
+        if (lr.positionCount == 0) return;
+
+        lr.SetPosition(0, castPoint.position);
+        lr.SetPosition(1, grapplePoint);
+    }
+
+    void StopGrapple()
+    {
+        lr.positionCount = 0;
+    }
+}
