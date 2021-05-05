d7b2a8899717de75b9e2d6f9b33b10af68ee2941
valter100 <va6648li-s@student.lu.se>
2021-05-05 22:37:49 +0200

    Update MyNetworkManager.cs

--- Assets/Scripts/Mirror/MyNetworkManager.cs
+++ Assets/Scripts/Mirror/MyNetworkManager.cs
@@ -155,7 +155,7 @@ public class MyNetworkManager : NetworkManager
     {
         if (sceneName == SelectedScene)
         {
-            playerPrefab = Characters[0]; //Here is where it is decided what character the player will spawn in as. Make it work with character select in lobby!
+            playerPrefab = Characters[1]; //Here is where it is decided what character the player will spawn in as. Make it work with character select in lobby!
             GamestateManager.gameIsOver = false;
             ballStartPos = GameObject.Find("BallSpawnPosition");
 
