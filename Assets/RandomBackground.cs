c37afd4488d971ddec57947b5f4bac799114a379
MaxR-L <maxrosquistlarsson@gmail.com>
2021-05-06 15:54:37 +0200

    Updated

--- Assets/RandomBackground.cs
+++ Assets/RandomBackground.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RandomBackground : MonoBehaviour
 {
     public Material[] skyboxes;
+    [SerializeField] GameObject light;
 
     // Use this for initialization
     void Start()
@@ -21,5 +22,29 @@ public class RandomBackground : MonoBehaviour
     {
         int x = Random.Range(0, skyboxes.Length - 1);
         RenderSettings.skybox = skyboxes[x];
+
+        if (x == 0)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + 31f,
+           transform.localEulerAngles.y + 124f, transform.localEulerAngles.z);
+        }
+
+        if (x == 1)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + 25f,
+           transform.localEulerAngles.y + 185f, transform.localEulerAngles.z + 3f);
+        }
+
+        if (x == 2)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + 25f,
+           transform.localEulerAngles.y + 185f, transform.localEulerAngles.z + 3f);
+        }
+
+        if (x == 3)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + 30f,
+           transform.localEulerAngles.y + 175.5f, transform.localEulerAngles.z + (-9f));
+        }
     }
 }
