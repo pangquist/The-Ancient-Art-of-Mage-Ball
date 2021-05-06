9b473de914550a1995d4773230175ff4eb1bd421
valter100 <va6648li-s@student.lu.se>
2021-05-06 14:57:13 +0200

    Update Amplify.cs

--- Assets/Scripts/Characters/Force Manipulator/Amplify.cs
+++ Assets/Scripts/Characters/Force Manipulator/Amplify.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Amplify : NetworkBehaviour
 {
-    // New amplify!
     [SerializeField] Camera mainCamera;
     [SerializeField] GameObject amplifyBeam;
     [SerializeField] float range;
     [SerializeField] float duration;
+    [SerializeField] Vector3 force;
     float currentDuration;
-    
+
     RaycastHit hit;
     GameObject ball;
 
@@ -22,13 +22,51 @@ public class Amplify : NetworkBehaviour
         {
             return;
         }
-        
+
         CmdSpawnBeam();
     }
 
     [Command]
     void CmdSpawnBeam()
     {
+        //ServerActivateBeam();
+        RpcActivateBeam();
+    }
+
+    [Server]
+    void ServerActivateBeam()
+    {
+        RpcActivateBeam();
+    }
+
+    [ClientRpc]
+    void RpcActivateBeam()
+    {
         amplifyBeam.SetActive(true);
+        amplifyBeam.GetComponent<AmplifyBeamBehaviour>().SetLifeTime(duration);
+        amplifyBeam.GetComponent<AmplifyBeamBehaviour>().SetForce(force);
+    }
+
+    [Client]
+    public void MoveBall(GameObject ball, Vector3 _force)
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        CmdMoveBall(ball, _force);
+    }
+
+    [Command]
+    public void CmdMoveBall(GameObject ball, Vector3 _force)
+    {
+        RpcMoveBall(ball, _force);
+    }
+
+    [ClientRpc]
+    void RpcMoveBall(GameObject ball, Vector3 _force)
+    {
+        ball.GetComponent<Rigidbody>().AddForce(_force);
     }
 }
