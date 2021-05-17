aac8b34ba23eb76a84b03c3ed272ac1ed3751084
Filosof93 <Kasper-93@hotmail.se>
2021-05-17 17:37:50 +0200

    Update LobbyMenu.cs

--- Assets/Scripts/Main menu/LobbyMenu.cs
+++ Assets/Scripts/Main menu/LobbyMenu.cs
@@ -118,15 +118,22 @@ public class LobbyMenu : MonoBehaviour
             }
         }
 
-
-        if(redPlayers >= 3 && bluePlayers < 3)
+        if (NetworkClient.ready)
         {
-            newPlayer.CmdSetTeamName("Blue Team");
+            if (redPlayers >= 3 && bluePlayers < 3)
+            {
+                newPlayer.CmdSetTeamName("Blue Team");
+            }
+            else
+            {
+                newPlayer.CmdSetTeamName("Red Team");
+            }
         }
         else
         {
-            newPlayer.CmdSetTeamName("Red Team");
+            Debug.Log("NetworkClient is not ready (LobbyMenu)!");
         }
+        
 
         startGameButton.interactable = menuPlayers.Count >= MyNetworkManager.playersRequiredToStart; 
     }
