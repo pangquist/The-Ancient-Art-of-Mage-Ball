7ebcbf91fd93f42fbf2560110954f73701324490
valter100 <va6648li-s@student.lu.se>
2021-06-01 12:44:53 +0200

    Update MyNetworkMenuPlayer.cs

--- Assets/Scripts/Mirror/MyNetworkMenuPlayer.cs
+++ Assets/Scripts/Mirror/MyNetworkMenuPlayer.cs
@@ -32,7 +32,7 @@ public class MyNetworkMenuPlayer : NetworkBehaviour
     [SyncVar(hook = nameof(AuthorityHandlePartyOwnerStateUpdated))]
     bool isPartyOwner = false;
 
-    [SyncVar(hook = nameof(HandleSteamIdUpdated))]
+    //[SyncVar(hook = nameof(HandleSteamIdUpdated))]
     ulong steamId;
 
     [SyncVar(hook = nameof(HandlePlayerColorUpdated))]
@@ -76,6 +76,16 @@ public class MyNetworkMenuPlayer : NetworkBehaviour
         if (steamId == 0) //THIS SHOULD BE REMOVED WHEN TESTING FOR REAL
         {
             steamId = 1;
+        }
+
+        if (steamId != 1)
+        {
+            var CSteamID = new CSteamID(steamId);
+            CmdSetDisplayName(SteamFriends.GetFriendPersonaName(CSteamID));
+        }
+        else
+        {
+            CmdSetDisplayName($"Player 1");
         }
     }
 
