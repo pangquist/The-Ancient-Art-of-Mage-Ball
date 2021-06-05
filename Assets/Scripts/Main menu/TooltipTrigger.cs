8e62ea3f7f5005aef5635c59a5cd4e1fbf2a6aee
emilpasse <emil@passe.se>
2021-06-05 18:47:00 +0200

    Tooltip fix

--- Assets/Scripts/Main menu/TooltipTrigger.cs
+++ Assets/Scripts/Main menu/TooltipTrigger.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+
 public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("References")]
     [SerializeField] GameObject tooltipBackground;
+
+    [SerializeField] GameObject characterNameGO;
+    [SerializeField] Tooltip characterName;
+    [SerializeField] string characterNameString;
 
     [Header("Lists")]
     [SerializeField] List<string> headers;
@@ -15,6 +20,7 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     [SerializeField] List<Tooltip> tooltips;
     [SerializeField] List<Sprite> abilityImages;
     [SerializeField] List<GameObject> tooltipsGO;
+    [SerializeField] GameObject Icons;
 
     public void Awake()
     {
@@ -27,20 +33,25 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     public void OnPointerEnter(PointerEventData eventData)
     {
         tooltipBackground.SetActive(true);
-        for (int i = 0; i < tooltips.Count-1; i++)
+        for (int i = 0; i < tooltips.Count; i++)
         {
+            Icons.SetActive(true);
             tooltips[i].ShowToolTip(tooltipsGO[i], tooltips[i], contents[i], abilityImages[i], headers[i]);
+            Debug.Log("Sätter igång tooltip");
         }
+        characterName.ShowCharacterName(characterNameGO, characterName, characterNameString);
     }
- 
+
 
     public void OnPointerExit(PointerEventData eventData)
     {
         tooltipBackground.SetActive(false);
         for (int i = 0; i < tooltips.Count; i++)
         {
+            Icons.SetActive(false);
             tooltips[i].HideToolTip(tooltipsGO[i]);
         }
+        characterName.HideToolTip(characterNameGO);
     }
 }
 
